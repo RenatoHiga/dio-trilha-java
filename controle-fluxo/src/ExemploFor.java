99ff0422022f99163cb1c0092363dda8d36f0192
renato <renatoka14@gmail.com>
2024-08-08 15:15:24 -0300

    Loop structures

---
+++ controle-fluxo/src/ExemploFor.java
@@ -0,0 +1,12 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+
+        // no array o índice inicia em 0
+        String alunos [] = { "FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+        for (String aluno : alunos) {
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+    }
+
+}
